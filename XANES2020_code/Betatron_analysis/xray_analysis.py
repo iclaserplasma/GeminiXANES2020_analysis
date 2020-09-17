147231c5f0e7afb5c6f6b63177c7af3862e2a5a8
Matthew Streeter <22773109+mjvstreeter@users.noreply.github.com>
2020-09-17 14:24:21 +0100

    Update xray_analysis.py
    Tidies up script, and added comments.
    Also added feature to speicify which filters to include in analysis
    rename filter_number to fList_filter_number to modify confusion levels

--- XANES2020_code/Betatron_analysis/xray_analysis.py
+++ XANES2020_code/Betatron_analysis/xray_analysis.py
@@ -6,11 +6,22 @@ from scipy.interpolate import interp1d
 from scipy.special import kv
 import pickle
 
-def load_object(filename):
-    with open(filename, 'rb') as fid:
-        return pickle.load(fid)
+from XANES2020_code.general_tools import load_object
 
 class fRegions(object):
+    """ Class used for each filter number to store material, thicnkess and transmission functions
+    is initialised when specifying the filter and has methods to calculate the theoretical transmission
+    as well as to measure the transmission of a given image and associated labelled mask
+    label (int) is a number which is used to identify the filter in a labelled mask
+    d (float) is the material thickness in microns
+    dErr (float) is the error on that thickness
+    E_keV (array) is the energy axis used for calculations
+    mu (array) is the photon attentuation coefficient as a function of E_keV in cm^2/g
+    rho (float) is the density in g/cm^3
+    T_E (array) is the transmission as a function of E_keV and can be calculated from the method calcT_E
+    fTrans (float) is the average tranmsission of the filter, can be calculated with calcTrans
+    fTrans_rms (float) is the error on fTrans, also calculated with the method calcTrans
+    """
     def __init__(self, label =0, material ='none', d =0, dErr =0, E_keV = [], mu = [], rho = 0,T_E = 0,fTrans=0,fTrans_rms=0):
         self.label = label
         self.material = material
@@ -24,25 +35,32 @@ class fRegions(object):
         self.fTrans_rms = fTrans_rms
         
     def calcCentroid(self,filterMask):
+        """ Find the x,y centroid of the filter using filterMask
+        """
         [yInd,xInd] = np.where(filterMask==self.label)
         yC = np.mean(yInd)
         xC = np.mean(xInd)
         return xC,yC
     
     def calcTrans(self,imgT,filterMask):
-        #[yInd,xInd] = np.where(filterMask==self.label)
+        """ Get the mean transmission (and RMS) of the pixels of imgT specified by where filterMask matches the label
+            Not sure if RMS is the relavent error as we would really like the error on the tranmission measurment
+        """
         f_sel = filterMask==self.label
         self.fTrans = np.mean(imgT[f_sel])
         self.fTrans_rms = np.sqrt(np.mean((self.fTrans-imgT[f_sel])**2))
-        # self.fTrans_std_err = self.fTrans_rms/np.sqrt(np.sum(filterMask==self.label))
         tLabel = np.mean(filterMask[f_sel])
         return self.fTrans, self.fTrans_rms, tLabel
 
     def calcT_E(self):
+        """ Calculate the theoretical transmission of the filter as a function of E_keV
+        """
         self.T_E =  np.exp(-self.rho*self.mu*self.d*1e-4)
         return self.T_E
 
     def calcTransCumDist(self):
+        """ Used for monte-carlo error analysis. Usesd the measurement error set up a statistical model for the measurements
+        """
         nT = 1000
         tAxis = np.linspace(-10,10,num=nT)*self.fTrans_rms+self.fTrans
         fTransExp = np.exp(-(tAxis-self.fTrans)**2/(2*self.fTrans_rms**2))
@@ -52,16 +70,18 @@ class fRegions(object):
         
         self.tAxisCumDist = yCS
         self.tAxis = tAxis[ia]
-
         self.randomTransFunc()
 
         return self.tAxis, self.tAxisCumDist
     
     def randomTransFunc(self):
+        """ Get a randomised transmission value from the distribution from calcTransCumDist
+        """
         f = interp1d(self.tAxisCumDist,self.tAxis)
         self.calcModifiedTrans = f
         return f
 
+## These classes were a bad idea, but it's a bit late to change them now.
 class Mask(object):
     def __init__(self, name, value):
         self.name = name
@@ -78,12 +98,34 @@ class Beam(object):
         self.original_file = original_file
         self.beamCounts = beamCounts
 
-
+# interpolate bessel function rather than calculate each time for speed
 x = np.logspace(-5,2,num=1000,endpoint=True)
 y = (x*kv(2/3,x))**2
 I_Xi = interp1d(x,y,bounds_error=False,fill_value=0)
 
 class Betatron_spec_fitter():
+    """ Class for fitting tranmsission values to get E_crit
+    transmission_img (2D array) are values ideally between 0 and 1 of relative transmission of beam
+    filter_obj is an dictionary created by a calibration which contains information about each filter 
+        and also about the other material in the  beamline
+    filter_obj = {
+    'transmission_functions': trans_funcs,
+    'filter_arrangement': filter_arangement,
+    'synthetic_filters': fake_filters,
+    'synthetic_transmission': fake_trans,
+    'filter_fRegions': fList,
+    'null_fRegions': nullList,
+    }
+
+    mask_obj is a dictionary containing details of where the beam and filter regions are located on the image
+    mask_obj = {
+    'beam_mask':beam_mask_final,
+    'filter_label_regions': labelled,
+    'filter_number_regions': img_filters,
+    'filter_label': img_filter_labels,
+    'filter_number':  img_filter_numbers,    
+    }
+    """
     def __init__(self,transmission_img,filter_obj,mask_obj):
         self.filter_obj = filter_obj
         self.transmission_img = transmission_img
@@ -94,46 +136,34 @@ class Betatron_spec_fitter():
     def _load_info(self):
         transFuns = self.filter_obj['transmission_functions']
         self.fList = self.filter_obj['filter_fRegions']
+        self.fList_filter_number = [x.label for x in self.fList]
         self.E_keV = transFuns[0].E_keV
         self.aQE = transFuns[0].T_E
         self.nE = np.size(self.E_keV)
         self.transMat = transFuns[2].T_E
         self.nullTrans = transFuns[1].T_E
 
-    def measure_transmission(self,method='fit'):
-        if method.lower() in 'quick':
-            Ny,Nx = np.shape(self.transmission_img)
-            X,Y = np.meshgrid(np.arange(Nx),np.arange(Ny))
-            
+    def measure_transmission(self):
+       
         for fReg in self.fList:
-            if method.lower() in 'quick':
-                reg_labels = np.unique(self.mask_obj['filter_label_regions'][(self.mask_obj['filter_number_regions']==fReg.label)])
-                reg_trans = []
-                reg_trans_rms = []
-                for l in reg_labels:
-                    f_sel = self.mask_obj['filter_label_regions']==l
-                    yInd,xInd = np.where(f_sel)
-                    c_y = np.mean(yInd)
-                    c_x = np.mean(xInd)
-                    R = np.sqrt((Y-c_y)**2+(X-c_x)**2)
-                    R_max = np.min(R[self.mask_obj['beam_mask']>0])*1.5
-                    b_sel = (R<R_max)*(self.mask_obj['beam_mask']>0)
-
-                    reg_b_mean = np.mean(self.transmission_img[b_sel])
-                    reg_b_rms = np.std(self.transmission_img[b_sel])
-                    reg_f_mean = np.mean(self.transmission_img[f_sel])
-                    reg_f_rms = np.std(self.transmission_img[f_sel])
-                    reg_trans.append(reg_f_mean/reg_b_mean)
-                    reg_trans_rms.append(np.sqrt((reg_f_rms/reg_f_mean)**2+(reg_b_rms/reg_b_mean)**2)*reg_f_mean/reg_b_mean)
-
-                fReg.fTrans = np.mean(reg_trans)
-                fReg.fTrans_rms = np.mean(reg_trans_rms)
-            elif method.lower() in 'fit':
-                fReg.calcTrans(self.transmission_img,self.mask_obj['filter_number_regions'])
+            fReg.calcTrans(self.transmission_img,self.mask_obj['filter_number_regions'])
             fReg.calcTransCumDist()
         self.measured_trans = np.array([x.fTrans for x in self.fList])
         self.measured_trans_rms = np.array([x.fTrans_rms for x in self.fList])
-        self.filter_number = [x.label for x in self.fList]
+        
+
+    def measure_tranmission_per_block(self):
+        filter_label_regions = self.mask_obj['filter_label_regions']
+        filter_labels = self.mask_obj['filter_label']
+        filter_number = self.mask_obj['filter_number']
+        f_trans = []
+        f_trans_rms = []
+        for f_label in filter_labels:
+            f_trans.append(np.mean(self.transmission_img[filter_label_regions==f_label]))
+            f_trans_rms.append(np.std(self.transmission_img[filter_label_regions==f_label]))
+        self.block_trans = np.array(f_trans)
+        self.block_trans_rms = np.array(f_trans_rms)
+        self.block_filter_number = filter_number
 
     def theoretical_trans(self,E_c):
         S = 1.0*I_Xi(self.E_keV/(2*E_c))
@@ -148,11 +178,30 @@ class Betatron_spec_fitter():
         return total_signal
         
     def err_func(self,E_c):
-        err = self.theoretical_trans(E_c) - self.measured_trans
+        theory_trans = self.theoretical_trans(E_c) 
+        if self.by_block:
+            theory_trans = theory_trans[self.block_filter_number-1]
+            err = theory_trans - self.block_trans
+
+            if self.f_sel is not None:
+                f_test = []
+                for n in self.block_filter_number:
+                    f_test.append(np.any(self.f_sel==n))
+                err = err[f_test]
+        else:
+            err = self.theoretical_trans(E_c) - self.measured_trans
+            
+            if self.f_sel is not None:
+                err = err[self.f_sel]
         return np.sqrt(np.mean(err**2))
     
-    def calc_E_crit(self,method='fit'):   
-        self.measure_transmission(method=method)
+    def calc_E_crit(self,by_block=False,f_sel=None):   
+        self.by_block = by_block
+        self.f_sel = f_sel
+        if by_block:
+            self.measure_tranmission_per_block()
+        else:
+            self.measure_transmission()
         res = minimize(self.err_func,(10),method='Nelder-Mead', tol=1e-4)
         E_c = res.x[0]
         self.trans_pred = self.theoretical_trans(E_c)
