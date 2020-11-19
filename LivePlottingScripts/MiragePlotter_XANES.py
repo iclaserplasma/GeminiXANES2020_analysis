df51c84c3041c9455ab86ab6d75e17afdcde4005
iclaserplasma <42937440+iclaserplasma@users.noreply.github.com>
2020-11-19 17:10:57 +0000

    Experimental changes

--- LivePlottingScripts/MiragePlotter_XANES.py
+++ LivePlottingScripts/MiragePlotter_XANES.py
@@ -125,10 +125,12 @@ for diag in andor_diag_list:
     win.add_line_plot(diag+' lineout', diag, dx420_mean_x)
     server.diag_list.addItem(diag)
 
-win.add_image_plot('Pinhole', 'Pinhole', dx420_img)
-win.add_line_plot('Pinhole'+' x lineout', 'Pinhole', dx420_mean_x)
-win.add_line_plot('Pinhole'+' y lineout', 'Pinhole', dx420_mean_y)
-server.diag_list.addItem('Pinhole')
+pinhole_diag_list = ['Pinhole', 'PinholeAl']
+for p in pinhole_diag_list:
+    win.add_image_plot(p, p, dx420_img)
+    win.add_line_plot(p+' x lineout', p, dx420_mean_x)
+    win.add_line_plot(p+' y lineout', p, dx420_mean_y)
+    server.diag_list.addItem(p)
 
 # --------- HAPG
 
